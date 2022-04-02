1098f21d4bf8549875f371247e9c3f8d8106a5fc
mylesholloway1 <mylesholloway1@gmail.com>
2022-04-02 19:26:48 -0400

    connected to mongo db

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+connectDB();
 
 app.get('/', (req,res)=>res.send('API has been created'));
 
