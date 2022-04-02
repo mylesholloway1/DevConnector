528643176faa2d29bc4ef9a3266d87e4f4ab56f4
mylesholloway1 <mylesholloway1@gmail.com>
2022-04-02 18:51:28 -0400

    start practice

--- routes/api/post.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route  GET api/post
-// @desc   post route
-// @access Public
-router.get('/', (req,res)=>res.send('post route'));
-
-module.exports = router;
